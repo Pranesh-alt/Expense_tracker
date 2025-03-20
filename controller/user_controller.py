66b4c2fdb1696a5c8b67cadf19dbb12505cbf7df
Pranesh Raghumu vedhapandiyan <pvedhapandiyan@freshworks.com>
2025-03-20 18:26:16 +0530

    updated status_code

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,26 +1,26 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from services.user_services import update_user, get_user, get_users, delete_user, create_user
 from schemas import UserCreate, UserUpdate, UserResponse
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/", response_model=UserResponse)
+@router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def create__user(user: UserCreate):
     return create_user(user)
 
-@router.get("/", response_model=list[UserResponse])
+@router.get("/", response_model=list[UserResponse], status_code=status.HTTP_200_OK)
 def get__users():
     return get_users()
 
-@router.get("/{user_id}", response_model=UserResponse)
+@router.get("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
 def get__user(user_id: int):
     return get_user(user_id)
 
-@router.put("/{user_id}", response_model=UserResponse)
+@router.put("/{user_id}", response_model=UserResponse, status_code=status.HTTP_204_NO_CONTENT)
 def update__user(user_id:int,user_data: UserUpdate):
     return update_user(user_id, user_data)
 
-@router.delete("/{user_id}")
+@router.delete("/{user_id}", response_model=UserResponse, status_code=status.HTTP_204_NO_CONTENT)
 def delete__user(user_id: int):
     return delete_user(user_id)
